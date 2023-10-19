78556400f60d391b1e609e5ef0d66b861c642894
tordstr <tordstr@stud.ntnu.no>
2023-10-19 15:23:00 +0200

    change in utils

--- utils.py
+++ utils.py
@@ -4,5 +4,6 @@ import torch
 def get_schedule(T: int):
     b_t = torch.asarray([10**-4 + 0.02 / T * i for i in range(T)])
     a_t = 1 - b_t
+    A_t = torch.asarray([torch.prod(a_t[:t]) for t in range(T)])
 
-    return a_t, b_t
+    return a_t, b_t, A_t
