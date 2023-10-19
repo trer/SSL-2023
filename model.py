13927716610e43d452d011fe50563f72ce36e236
Øyvind Monsen <yvind.monsen@gmail.com>
2023-10-19 14:11:44 +0200

    add model

--- model.py
+++ model.py
@@ -1,2 +1,63 @@
-class MNISTDiffuser():
-    pass
+import torch
+import math
+
+
+def positionalencoding2d(d_model, height, width):
+    """
+    :param d_model: dimension of the model
+    :param height: height of the positions
+    :param width: width of the positions
+    :return: d_model*height*width position matrix
+    """
+    if d_model % 4 != 0:
+        raise ValueError(
+            "Cannot use sin/cos positional encoding with "
+            "odd dimension (got dim={:d})".format(d_model)
+        )
+    pe = torch.zeros(d_model, height, width)
+    # Each dimension use half of d_model
+    d_model = int(d_model / 2)
+    div_term = torch.exp(torch.arange(0.0, d_model, 2) * -(math.log(10000.0) / d_model))
+    pos_w = torch.arange(0.0, width).unsqueeze(1)
+    pos_h = torch.arange(0.0, height).unsqueeze(1)
+    pe[0:d_model:2, :, :] = (
+        torch.sin(pos_w * div_term).transpose(0, 1).unsqueeze(1).repeat(1, height, 1)
+    )
+    pe[1:d_model:2, :, :] = (
+        torch.cos(pos_w * div_term).transpose(0, 1).unsqueeze(1).repeat(1, height, 1)
+    )
+    pe[d_model::2, :, :] = (
+        torch.sin(pos_h * div_term).transpose(0, 1).unsqueeze(2).repeat(1, 1, width)
+    )
+    pe[d_model + 1 :: 2, :, :] = (
+        torch.cos(pos_h * div_term).transpose(0, 1).unsqueeze(2).repeat(1, 1, width)
+    )
+
+    return pe
+
+
+class MNISTDiffuser(torch.nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        # Simple CNN UNet architecture
+        # self.pos_embedding = SinusoidalPosEmb(dim)
+        self.encoder = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.decoder = torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(32, 32, kernel_size=2, stride=2),
+            torch.nn.ReLU(),
+            torch.nn.ConvTranspose2d(32, 1, kernel_size=2, stride=2),
+            torch.nn.ReLU(),
+        )
+
+    def forward(self, x, timestep):
+        # out = self.pos_embedding(x)
+        out = self.encoder(x)
+        out = self.decoder(x)
+        return out
