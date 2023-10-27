24f1fcd603e04bd89958505910c19934217453d1
Karl Erik Hofseth <karl@kehofseth.no>
2023-10-27 14:01:48 +0200

    Fix some CLI stuff

--- main.py
+++ main.py
@@ -3,21 +3,10 @@ import torch
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-import random
-
-
 from model import MNISTDiffuser
 from generator import get_dataloader
 from utils import device
 
-# Placeholder
-class PlaceholderDataLoader:
-    def __next__(self) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        if random.random() < 0.1:
-            raise StopIteration
-        return torch.tensor([[[[random.random()]*28]*28]]*100), torch.tensor([[[[random.random()]*28]*28]]*100), torch.tensor([[random.randint(0, 1000)]]*100)
-    def __iter__(self):
-        return self
 
 loss_fn: torch.nn.MSELoss = torch.nn.MSELoss()
     
@@ -45,16 +34,16 @@ def train(model: MNISTDiffuser, epochs: int, learning_rate: float, momentum: flo
             outputs = model(inputs, timesteps)
             loss: torch.Tensor = loss_fn(outputs, labels)
             loss.backward()
-            
+
+            avg_loss = (avg_loss * i + loss.item()) / (i + 1)
             optimizer.step()
             
-            avg_loss = (avg_loss + loss.item()) / (i + 1)
             
         
         losses.append(avg_loss)
         
         if e % 1 == 0:
-            print(f'Epoch {e}  Loss: {losses[-1]:.2f}  Best: {min(losses):.2f}')
+            print(f'Epoch {e}  Loss: {losses[-1]:.6f}  Best: {min(losses):.6f}')
             
         
 
@@ -66,7 +55,7 @@ def main():
     parser.add_argument('-e', '--epochs', type=int, default=100)
     parser.add_argument('-l', '--load', help='Path to the model to load for further training')
     parser.add_argument('-o', '--out', help='Path to store the model', default='model')
-    parser.add_argument('-lr', '--learning-rate', dest='learning_rate', default=0.001)
+    parser.add_argument('-r', '--learning-rate', dest='learning_rate', type=float, default=0.001)
     parser.add_argument('-m', '--momentum', default=0.9)
     parser.add_argument('-b', '--batch', default=128, type=int)
     
