cdeb5bd46f7abe446ca5610577a5d6f53b310727
Øyvind Monsen <yvind.monsen@gmail.com>
2023-11-02 12:40:25 +0100

    model time emb

--- model.py
+++ model.py
@@ -1,51 +1,36 @@
 import torch
+from torch import nn
 import math
 from utils import get_schedule, device, normalize
 import matplotlib.pyplot as plt
 
 
-def positionalencoding2d(d_model, height, width):
-    """
-    :param d_model: dimension of the model
-    :param height: height of the positions
-    :param width: width of the positions
-    :return: d_model*height*width position matrix
-    """
-    if d_model % 4 != 0:
-        raise ValueError(
-            "Cannot use sin/cos positional encoding with "
-            "odd dimension (got dim={:d})".format(d_model)
-        )
-    pe = torch.zeros(d_model, height, width)
-    # Each dimension use half of d_model
-    d_model = int(d_model / 2)
-    div_term = torch.exp(torch.arange(0.0, d_model, 2) * -(math.log(10000.0) / d_model))
-    pos_w = torch.arange(0.0, width).unsqueeze(1)
-    pos_h = torch.arange(0.0, height).unsqueeze(1)
-    pe[0:d_model:2, :, :] = (
-        torch.sin(pos_w * div_term).transpose(0, 1).unsqueeze(1).repeat(1, height, 1)
-    )
-    pe[1:d_model:2, :, :] = (
-        torch.cos(pos_w * div_term).transpose(0, 1).unsqueeze(1).repeat(1, height, 1)
-    )
-    pe[d_model::2, :, :] = (
-        torch.sin(pos_h * div_term).transpose(0, 1).unsqueeze(2).repeat(1, 1, width)
-    )
-    pe[d_model + 1 :: 2, :, :] = (
-        torch.cos(pos_h * div_term).transpose(0, 1).unsqueeze(2).repeat(1, 1, width)
-    )
+def sinusoidal_embedding(n, d):
+    # Returns the standard positional embedding
+    embedding = torch.zeros(n, d)
+    wk = torch.tensor([1 / 10_000 ** (2 * j / d) for j in range(d)])
+    wk = wk.reshape((1, d))
+    t = torch.arange(n).reshape((n, 1))
+    embedding[:, ::2] = torch.sin(t * wk[:, ::2])
+    embedding[:, 1::2] = torch.cos(t * wk[:, ::2])
 
-    return pe.to(device=device)
+    return embedding
 
 
 class MNISTDiffuser(torch.nn.Module):
-    def __init__(self, n_timesteps, dim):
+    def __init__(self, n_timesteps, dim, time_emb_dim=100):
         super().__init__()
         # Simple CNN UNet architecture
         # self.pos_embedding = SinusoidalPosEmb(dim)
         self.dim = dim
+        # Sinusoidal embedding
+        self.time_embed = torch.nn.Embedding(n_timesteps, self.dim)
+        self.time_embed.weight.data = sinusoidal_embedding(n_timesteps, time_emb_dim)
+        self.time_embed.requires_grad_(False)
+
+        # First half
+        self.te1 = self._make_te(time_emb_dim, 1)
         self.n_timesteps = n_timesteps
-        self.pos_embedding = positionalencoding2d(n_timesteps, dim, dim)
         self.block1 = torch.nn.Sequential(
             torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
@@ -55,13 +40,11 @@ class MNISTDiffuser(torch.nn.Module):
         self.down1 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.block2 = torch.nn.Sequential(
-            torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
-            torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
-            torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(),
-            torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
         )
 
@@ -70,25 +53,25 @@ class MNISTDiffuser(torch.nn.Module):
         # bottle neck
 
         self.bottleneck = torch.nn.Sequential(
-            torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
-            torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
         )
 
         # Up
-        self.up1 = torch.nn.ConvTranspose2d(32, 32, kernel_size=2, stride=2)
+        self.up1 = torch.nn.ConvTranspose2d(128, 128, kernel_size=2, stride=2)
         self.block3 = torch.nn.Sequential(
-            torch.nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(128 + 64, 64, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
-            torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
         )
 
-        self.up2 = torch.nn.ConvTranspose2d(32, 32, kernel_size=2, stride=2)
+        self.up2 = torch.nn.ConvTranspose2d(64, 64, kernel_size=2, stride=2)
 
         self.block4 = torch.nn.Sequential(
-            torch.nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(64 + 32, 32, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
             torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
@@ -107,18 +90,27 @@ class MNISTDiffuser(torch.nn.Module):
                 a_t_bar = a[:t].prod()
                 b_t = b[t]
                 sigma = torch.sqrt(b_t)
-                noise = self.forward(x, t)
+                time = torch.tensor([t])
+                noise = self.forward(x, time)
+                if t == 1:
+                    z = torch.zeros_like(z)
                 x = (1 / torch.sqrt(a_t)) * (
                     x - b_t / (torch.sqrt(1 - a_t_bar)) * noise
                 ) + sigma * z
 
         return x
 
-    def forward(self, x, timestep):
-        # TODO: Actually use the timestep
-        out1 = self.block1(x)
+    def forward(self, x: torch.tensor, t: torch.tensor):
+        n = len(x)
+        t = self.time_embed(t.to(x.get_device()).long())
+        out1 = self.block1(x + self.te1(t).reshape(n, -1, 1, 1))
         out2 = self.block2(self.down1(out1))
         out3 = self.bottleneck(self.down2(out2))
         out4 = self.block3(torch.cat([self.up1(out3), out2], dim=1))
         out5 = self.block4(torch.cat([self.up2(out4), out1], dim=1))
         return self.final(out5)
+
+    def _make_te(self, dim_in, dim_out):
+        return nn.Sequential(
+            nn.Linear(dim_in, dim_out), nn.SiLU(), nn.Linear(dim_out, dim_out)
+        )
